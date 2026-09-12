aa74285ed25d582b1c98971e3205188a2a92596f
Nguyễn Hoàng Nam <nemnguyenhoang2907@gmail.com>
2026-09-12 14:48:36 +0700

    Feat : Fix library errors and have swagger UI

--- Fire3D/Fire3D.API/Program.cs
+++ Fire3D/Fire3D.API/Program.cs
@@ -3,7 +3,6 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
@@ -12,6 +11,13 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/openapi/v1.json", "Fire3D API v1");
+        options.RoutePrefix = "swagger";
+    });
+
 }
 
 app.UseHttpsRedirection();
--- Fire3D/Fire3D.API/obj/Debug/net10.0/Fire3D.API.AssemblyInfo.cs
+++ Fire3D/Fire3D.API/obj/Debug/net10.0/Fire3D.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Fire3D.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+30164a2ac77ed5a17d7c906838483090c2bb28e4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+247f0bb51a40fda1ed7dccdd60b70e4b104f34b1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Fire3D.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Fire3D.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
