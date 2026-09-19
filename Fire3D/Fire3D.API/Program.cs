a1e0d6500ada89f543fae16b2c45af847a53d16d
Nguyễn Hoàng Nam <nemnguyenhoang2907@gmail.com>
2026-09-19 14:25:18 +0700

    style: inject custom swagger synthwave css

--- Fire3D/Fire3D.API/Program.cs
+++ Fire3D/Fire3D.API/Program.cs
@@ -68,6 +68,7 @@ app.UseSwaggerUI(options =>
 {
     options.SwaggerEndpoint("/openapi/v1.json", "Fire3D API v1");
     options.RoutePrefix = "swagger";
+    options.InjectStylesheet("/css/swagger-synthwave.css");
 });
 
 
